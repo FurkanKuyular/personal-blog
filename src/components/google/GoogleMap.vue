4d396b7643efd3997cb25074e07806e35decc1f7
Furkan Kuyular <furkankuyular@192.168.1.37>
2023-09-06 16:09:25 +0300

    Remove geoLocation google integration for map && use directly iframe for google maps

--- src/components/google/GoogleMap.vue
+++ src/components/google/GoogleMap.vue
@@ -1,47 +1,13 @@
+<template>
+  <div style="width: 100%">
+    <iframe width="300" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=300&amp;height=350&amp;hl=en&amp;q=Ka%C4%9F%C4%B1thane%20H%C3%BCrriyet%20Mahallesi+(Address)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+      <a href="https://www.maps.ie/population/">Population Estimator map</a>
+    </iframe>
+  </div>
+</template>
+
 <script>
-/* eslint-disable no-undef */
-import { computed, ref, onMounted, onUnmounted, watch } from 'vue'
-import { useGeolocation } from '@/objects/google/GoogleMapLoader'
-import { Loader } from '@googlemaps/js-api-loader'
-
-const GOOGLE_MAPS_API_KEY = 'AIzaSyDLjaYFtRs507chIw-TF-72Me_jRaHMh1w'
-
 export default {
   name: 'App',
-  setup() {
-    const { coords } = useGeolocation()
-
-    const currPos = computed(() => ({
-      lat: coords.value.latitude,
-      lng: coords.value.longitude
-    }))
-    const otherPos = ref(null)
-
-    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY })
-    const mapDiv = ref(null)
-    let map = ref(null)
-    let clickListener = null
-    onMounted(async () => {
-      await loader.load()
-      map.value = new google.maps.Map(mapDiv.value, {
-        center: currPos.value,
-        zoom: 7
-      })
-      clickListener = map.value.addListener(
-          'click',
-          ({ latLng: { lat, lng } }) =>
-              (otherPos.value = { lat: lat(), lng: lng() })
-      )
-    })
-    onUnmounted(async () => {
-      if (clickListener) clickListener.remove()
-    })
-
-    return { mapDiv }
-  }
 }
 </script>
-
-<template>
-  <div ref="mapDiv" style="width: 300px; height: 350px" />
-</template>
